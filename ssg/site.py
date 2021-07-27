ecc8176d67452df129d82e2d825d1bed671c3c14
Shubham Singh <Shubham.Singh4@FISGLOBAL.COM>
2021-07-27 22:31:16 +0530

    updated code

--- ssg/site.py
+++ ssg/site.py
@@ -21,14 +21,13 @@ class Site:
     def create_dir(self, path):
         '''
         '''
-        p = PurePosixPath(path)
-        directory = f'{self.dest}/{p.relative_to(self.source)}'
-        Path.mkdir(directory, parents=True, exist_ok=True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
     
     def build(self):
         '''
         '''
-        Path.mkdir(self.dest, parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
